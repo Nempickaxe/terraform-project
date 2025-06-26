88f79b1172985ccdbc45c60af0656b918f2a7e71
Nem_pickaxe <nemishkanwar@gmail.com>
2025-06-27 01:00:20 +0530

    Add output for EC2 public IP SSH command
    Introduced a Terraform output that provides the SSH command to connect to the EC2 instance using its public IP. This helps users quickly obtain the correct SSH command after provisioning.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -82,3 +82,8 @@ resource "aws_security_group" "tf_ec2_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+output "ec2_public_ip" {
+  value = "ssh -i ~/.ssh/terraform-ec2.pem ubuntu@${aws_instance.tf_ec2_instance.public_ip}"
+  
+}
