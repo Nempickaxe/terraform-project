036764d19985265eb5b1dbaa00c16da940395fbc
Nem_pickaxe <nemishkanwar@gmail.com>
2025-06-27 00:56:31 +0530

    Add user_data script to EC2 instance
    Introduces a user_data script to automate setup of a Node.js application on EC2 launch. The script clones a GitHub repo, installs Node.js and npm, sets up environment variables, and installs dependencies. Also enables user_data_replace_on_change for updates.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,6 +14,29 @@ resource "aws_instance" "tf_ec2_instance" {
   vpc_security_group_ids = [ aws_security_group.tf_ec2_sg.id ] # attach security group
   key_name = "terraform-ec2"
   depends_on = [ aws_s3_bucket.tf_s3_bucket ]
+  user_data = <<-EOF
+                #!/bin/bash
+                
+                git clone https://github.com/verma-kunal/nodejs-mysql.git /home/ubuntu/nodejs-mysql
+                cd /home/ubuntu/nodejs-mysql
+
+                # Install Node.js and npm
+                sudo apt update -y
+                sudo apt install -y nodejs npm
+
+                # edit env vars
+                echo "DB_HOST=" | sudo tee -a .env
+                echo "DB_USER=" | sudo tee -a .env
+                echo "DB_PASS=" | sudo tee -a .env
+                echo "DB_NAME=" | sudo tee -a .env
+                echo "TABLE_NAME=" | sudo tee -a .env
+                echo "PORT=" | sudo tee -a .env
+
+                # start server
+                npm install
+                EOF
+    
+  user_data_replace_on_change = true
 
   tags = {
     Name = "NodeJS-server"
